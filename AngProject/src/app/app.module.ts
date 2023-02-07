22dc1ff91bd3a674af250f344d0bc8414d1a6a39
Max Karpuk <maksim.karpuk13910@gmail.com>
2023-02-08 00:48:14 +0300

    feature-1

--- AngProject/src/app/app.module.ts
+++ AngProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
